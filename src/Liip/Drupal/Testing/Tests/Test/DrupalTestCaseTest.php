b234cf3422dcb560cb889c9da9b1db602fe149c0
Dan <dan@dc-home.ch>
2012-05-23 14:06:34 +0200

    Cleanup

--- src/Liip/Drupal/Testing/Tests/Test/DrupalTestCaseTest.php
+++ src/Liip/Drupal/Testing/Tests/Test/DrupalTestCaseTest.php
@@ -4,33 +4,36 @@ namespace Liip\Drupal\Testing\Test;
 
 use Liip\Drupal\Testing\Test\DrupalTestCase;
 
+// TODO: remove binding to my local install
 define ('DRUPAL_ROOT', '/home/dev/drupal-test/src');
 
 class DrupalTestCaseTest extends DrupalTestCase
 {
     public function __construct()
     {
-        parent::__construct('http://drupal-test.lo');
+        // TODO: remove binding to my local install
+        $baseUrl = 'http://drupal-test.lo';
+        parent::__construct($baseUrl);
     }
 
     public function testDrupalLoginLogout()
     {
         // Try to get an unauthorized page
-        $this->client->request('GET', 'http://drupal-test.lo/node/add');
+        $this->client->request('GET', $this->baseUrl . '/node/add');
         $this->assertResponseStatusEquals(403);
 
         // Login
         $this->drupalLogin('admin', '123123');
 
         // Check we can now get an unauthorized page
-        $this->client->request('GET', 'http://drupal-test.lo/node/add');
+        $this->client->request('GET', $this->baseUrl . '/node/add');
         $this->assertResponseStatusEquals(200);
 
         // Logout
         $this->drupalLogout();
 
         // Try to get an unauthorized page
-        $this->client->request('GET', 'http://drupal-test.lo/node/add');
+        $this->client->request('GET', $this->baseUrl . '/node/add');
         $this->assertResponseStatusEquals(403);
     }
 
